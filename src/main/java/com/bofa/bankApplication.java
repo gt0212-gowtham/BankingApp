52c8d5ecf2523c7777a4dd17abd413f0ba438a44
gt0212-gowtham <gowthamth599@gmail.com>
2025-11-12 21:13:13 -0600

    Update and rename BofaApplication.java to bankApplication.java

---
+++ src/main/java/com/bofa/bankApplication.java
@@ -0,0 +1,17 @@
+package com.bofa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.bofa.model.Customer;
+
+@SpringBootApplication
+public class BofaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BofaApplication.class, args);
+		
+		
+	}
+
+}
